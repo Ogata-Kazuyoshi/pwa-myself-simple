de59b57e1d02df94132e69449fdd85f3589b5485
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-02-20 18:02:42 +0900

    SCOPEの文字を削除

--- simple-pwa/src/App.tsx
+++ simple-pwa/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vitest!! + React</h1>
+      <h1>簡単なPWAアプリを作成</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
