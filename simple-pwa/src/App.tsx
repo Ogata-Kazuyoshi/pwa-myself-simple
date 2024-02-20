6d9f08f93252288a14eb81efdfd20e337efb53a5
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-02-20 20:46:52 +0900

    Install button

--- simple-pwa/src/App.tsx
+++ simple-pwa/src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { PWAInstallationButton } from './PWAInstallationButton';
+import Test from './Test';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,6 +19,8 @@ function App() {
         </a>
       </div>
       <h1>簡単なPWAアプリを作成</h1>
+      <Test />
+      <PWAInstallationButton />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ simple-pwa/src/PWAInstallationButton.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const PWAInstallationButton = () => {
+  const deferredPromptRef = useRef<BeforeInstallPromptEvent | null>(null);
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    const beforeInstallPromptHandler = (e: Event) => {
+      e.preventDefault(); // Prevent the default behavior
+      setReady(true);
+      deferredPromptRef.current = e as BeforeInstallPromptEvent;
+    };
+
+    window.addEventListener('beforeinstallprompt', beforeInstallPromptHandler);
+
+    return () => {
+      window.removeEventListener(
+        'beforeinstallprompt',
+        beforeInstallPromptHandler
+      );
+    };
+  }, []);
+
+  const handleClickInstall = async () => {
+    const deferredPrompt = deferredPromptRef.current;
+
+    if (deferredPrompt) {
+      // インストールプロンプトを表示する
+      await deferredPrompt.prompt();
+    }
+  };
+
+  return (
+    <>
+      <p>test</p>
+      <button disabled={!ready} onClick={handleClickInstall}>
+        Install
+      </button>
+    </>
+  );
+};
+
+interface BeforeInstallPromptEvent extends Event {
+  prompt: () => Promise<void>;
+  userChoice: Promise<{ outcome: 'accepted' | 'dismissed'; platform: string }>;
+}
