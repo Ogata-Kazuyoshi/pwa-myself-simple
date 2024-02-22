726262592dbb3324efc37a54f2c0a77c39247e2c
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-02-22 19:19:02 +0900

    slider追加

--- simple-pwa/src/App.tsx
+++ simple-pwa/src/App.tsx
@@ -1,14 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { PWAInstallationButton } from './PWAInstallationButton';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+import image1 from './assets/image1.png';
+import image2 from './assets/image2.png';
+import image3 from './assets/image3.png';
+import image4 from './assets/image4.png';
+import image5 from './assets/image5.png';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const splideOptions = {
+    type: 'loop',
+    perPage: 3,
+    focus: 'center',
+    padding: { left: '5rem', right: '5rem' },
+    gap: '2rem',
+    width: '100%',
+  };
   return (
-    <>
+    <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -19,18 +32,24 @@ function App() {
       </div>
       <h1>簡単なPWAアプリを作成</h1>
       <PWAInstallationButton />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Splide options={splideOptions}>
+        <SplideSlide>
+          <img src={image1} alt="Image 1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src={image2} alt="Image 2" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src={image3} alt="Image 3" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src={image4} alt="Image 3" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src={image5} alt="Image 3" />
+        </SplideSlide>
+      </Splide>
+    </div>
   );
 }
 
